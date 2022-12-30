dbcf14c917d86323c3da99f609d2fcaacf027d4b
Daniel151505 <daniel150520011@gmail.com>
2022-12-29 21:26:17 -0500

    Utilizar Gulp para crear imagenes Avif

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const autoprefixer = require('autoprefixer');
 //Imagenes
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css( done ) {
     //compilar sass
@@ -27,10 +28,24 @@ function imagenes() {
 }
 
 function versionWebp() {
+    const opciones = {
+        quality: 50
+    }
     return src('src/img/**/*.{png,jpg}')
-            .pipe( webp() )
+            .pipe( webp( opciones ) )
             .pipe( dest( 'build/img' ) );
 }
+
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png, jpg}')
+        .pipe( avif( opciones ) )
+        .pipe( dest('build/img') )
+    
+}
+
 function dev() {
     watch( 'src/scss/**/*.scss', css );
     watch( 'src/img/**/*', imagenes );
@@ -40,7 +55,8 @@ exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.default = series( imagenes, versionWebp, css, dev );
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev );
 
 // series: Se inicia una tarea, y hasta que finaliza, incia la siguiente.
 // parallel: Todas inician al mismo tiempo
