cfaed6321c3cc80d6614ead6b434387f1ab87ff6
Daniel151505 <daniel150520011@gmail.com>
2023-10-22 22:02:27 -0500

    Agregando cambios

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,32 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
-//CSS y SASS
+// CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss')
+const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
-//Imagenes
+// Imagenes
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
 function css( done ) {
-    //compilar sass
-    // 1: Identificar archivo, 2: Compilarla, 3: Guardar el .css
     src('src/scss/app.scss')
+        .pipe( sourcemaps.init() )
         .pipe( sass() )
-        .pipe( postcss([ autoprefixer() ]) )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.'))
         .pipe( dest('build/css') )
-    
+
     done();
 }
 
 function imagenes() {
     return src('src/img/**/*')
         .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') );
+        .pipe( dest('build/img') )
 }
 
 function versionWebp() {
@@ -32,18 +34,16 @@ function versionWebp() {
         quality: 50
     }
     return src('src/img/**/*.{png,jpg}')
-            .pipe( webp( opciones ) )
-            .pipe( dest( 'build/img' ) );
+        .pipe( webp( opciones ) )
+        .pipe( dest('build/img') )
 }
-
 function versionAvif() {
     const opciones = {
         quality: 50
     }
-    return src('src/img/**/*.{png, jpg}')
+    return src('src/img/**/*.{png,jpg}')
         .pipe( avif( opciones ) )
-        .pipe( dest('build/img') )
-    
+        .pipe( dest('build/img'))
 }
 
 function dev() {
@@ -51,12 +51,13 @@ function dev() {
     watch( 'src/img/**/*', imagenes );
 }
 
+
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev );
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
 
-// series: Se inicia una tarea, y hasta que finaliza, incia la siguiente.
-// parallel: Todas inician al mismo tiempo
+// series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+// parallel - Todas inician al mismo tiempo
